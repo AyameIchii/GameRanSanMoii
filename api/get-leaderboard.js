7157e422949e5767f50f21e03acea313617b2312
AyameIchii <157225128+AyameIchii@users.noreply.github.com>
2025-07-30 16:55:03 +0700

    Update get-leaderboard.js

--- api/get-leaderboard.js
+++ api/get-leaderboard.js
@@ -1,6 +1,31 @@
 // pages/api/get-leaderboard.js
-import { getScores } from '../../data/scores';
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).end();
 
-export default function handler(req, res) {
-  res.status(200).json(getScores());
+  const DETA_BASE_URL = process.env.DETA_BASE_URL;
+  const DETA_API_KEY = process.env.DETA_API_KEY;
+
+  const result = await fetch(`${DETA_BASE_URL}/query`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-Key': DETA_API_KEY
+    },
+    body: JSON.stringify({
+      "query": [],
+      "limit": 10
+    })
+  });
+
+  if (!result.ok) {
+    return res.status(500).json({ error: 'Không thể lấy danh sách' });
+  }
+
+  const data = await result.json();
+  const topScores = data.items
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10)
+    .map(({ name, score }) => ({ name, score }));
+
+  res.status(200).json(topScores);
 }

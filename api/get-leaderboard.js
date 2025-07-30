8d522b795ed8bc37f82ad74ec4e5fef1ee56a65a
AyameIchii <157225128+AyameIchii@users.noreply.github.com>
2025-07-30 16:22:20 +0700

    Update get-leaderboard.js

--- api/get-leaderboard.js
+++ api/get-leaderboard.js
@@ -1,3 +1,6 @@
+// pages/api/get-leaderboard.js
+import scores from '../../data/scores.js';
+
 export default function handler(req, res) {
-  res.status(200).json(scores || []);
+  res.status(200).json(scores);
 }

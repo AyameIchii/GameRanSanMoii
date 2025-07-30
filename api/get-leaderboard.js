a1574cef371c51a5f7c88ca168167e73c536eeea
AyameIchii <157225128+AyameIchii@users.noreply.github.com>
2025-07-30 16:12:01 +0700

    Update get-leaderboard.js

--- api/get-leaderboard.js
+++ api/get-leaderboard.js
@@ -1,10 +1,3 @@
-import { readFileSync, existsSync } from 'fs';
-import { join } from 'path';
-
-const file = join(process.cwd(), 'leaderboard.json');
-
 export default function handler(req, res) {
-  if (!existsSync(file)) return res.json([]);
-  const data = JSON.parse(readFileSync(file, 'utf8'));
-  return res.status(200).json(data);
+  res.status(200).json(scores || []);
 }
